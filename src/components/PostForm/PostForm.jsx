606d61699c8a5e403232dbb56317bb007dc164cf
kazuki19992 <kushida98@gmail.com>
2020-10-18 12:23:10 +0900

    まとめた

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -2,10 +2,12 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
-import PostText from './PostText';
+// import PostText from './PostText';
 // import DateTime from './DateTimePicker';
-import SendBtn from './SendBtn';
-import RemindTemplate from './RemindTemplate'
+// import SendBtn from './SendBtn';
+// import RemindTemplate from './RemindTemplate'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -14,7 +16,18 @@ const useStyles = makeStyles((theme) => ({
         left: 0,
         height: "100px",
         zIndex: 1000,
-      },
+    },
+    temp: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    text: {
+        '& .MuiTextField-root': {
+            margin: theme.spacing(1),
+            width: '25ch',
+        },
+    },
     }
 ));
 
@@ -32,12 +45,38 @@ export default function PostForm() {
                 <Grid container spacing={1}>
                     <Grid item xs={8}>
                         {/* <DateTime /><br /> */}
-                        <PostText />
+                        {/* <PostText /> */}
+                        <div className={classes.text}>
+                            <TextField
+                            id="outlined-multiline-static"
+                            label="入力してRiMAと話す"
+                            size="small"
+                            multiline
+                            rows={4}
+                            placeholder="リマインドをお願いする時は「展開」ボタンでテンプレートが展開されるよ！"
+                            variant="outlined"
+                            />
+                        </div>
                     </Grid>
                     <Grid item xs={4}>
-                        <RemindTemplate />
+                        {/* <RemindTemplate /> */}
+                        <div className={classes.temp}>
+                            <Button
+                            variant="contained"
+                            >
+                                展開
+                            </Button>
+                        </div>
                         {/* <br /> */}
-                        <SendBtn />
+                        {/* <SendBtn /> */}
+                        <div className={classes.temp}>
+                            <Button
+                            variant="contained"
+                            color="primary"
+                            >
+                                送信
+                            </Button>
+                        </div>
                     </Grid>
                 </Grid>
             </form>
